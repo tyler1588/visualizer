5dc88fcd48cb0c632bbf89b0e5af9c9a7454e11a
tyler1588 <87100297+tyler1588@users.noreply.github.com>
2023-08-24 20:36:08 -0400

    complete basic skeleton for visualization chart

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import './Navbar.css';
+
+const Navbar = () => {
+	return (
+		<div className='topnav'>
+			<a className='active' href='#home'>
+				Run
+			</a>
+			<a href='#news'>Reset</a>
+		</div>
+	);
+};
+
+export default Navbar;
